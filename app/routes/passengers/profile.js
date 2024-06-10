ea10c27b774452b1af677f8b0029bce8e5379957
adebayo.ayomide <adebayo.ayomide@konga.com>
2024-06-10 19:53:40 +0100

    Done: get profile and create profile

--- app/routes/passengers/profile.js
+++ app/routes/passengers/profile.js
@@ -5,59 +5,43 @@ const generateToken = require('../../utils/generateReferralCode')
 const checkAuth = require('../../middleware/auths')
 const jwt = require('jsonwebtoken')
 
-router.get('/', checkAuth, async (request, response) => {
+// Get passenger profile
+router.get('/', checkAuth, async (req, res) => {
   try {
-    const [_, token] = request.headers.authorization.split(' ')
-
-    const { id, email } = jwt.decode(token, process.env.SECRET_KEY)
-
+    const token = req.headers.authorization.split(' ')[1]
+    const { email } = jwt.decode(token, process.env.SECRET_KEY)
     const userPassenger = await passengers.findOne({
       where: { email },
       attributes: ['email', 'phoneNumber', 'city', 'lastName', 'firstName', 'id'],
     })
-
-    response.status(200).json(await userPassenger.getProfile())
+    const profile = await userPassenger.getProfile()
+    res.status(200).json(profile)
   } catch (error) {
     console.error(error)
-    response.status(401).json({ message: 'Unauthorized', status: false })
+    res.status(401).json({ message: 'Unauthorized', status: false })
   }
 })
 
-router.post('/', checkAuth, async (request, response) => {
+// Create passenger profile
+router.post('/', checkAuth, async (req, res) => {
   try {
-    const { Cars, TrustedBuddies, Card, shortBio } = request.body
-
-    const { userId } = jwt.decode(request.headers.authorization.split(' ')[1])
+    const { Cars, TrustedBuddies, Card, shortBio } = req.body
+    const userId = jwt.decode(req.headers.authorization.split(' ')[1]).userId
     const passenger = await passengers.findOne({ where: { id: userId } })
-
-    const passengerProfile = await passenger.createProfile({
+    const profile = await passenger.createProfile({
       role: 'passenger',
       shortBio,
       referralCode: generateToken(7),
     })
-
-    // const passengerProfile = await passenger.getProfile({ where: { role: 'passenger' } })
-    // console.log(passengerProfile)
-
-    Cars.forEach((car) => {
-      passengerProfile.createCar(car)
-    })
-
-    TrustedBuddies.forEach((buddies) => {
-      passengerProfile.createTrustedbuddy(buddies)
-    })
-
-    Card.forEach((card) => {
-      passengerProfile.createCard(card)
-    })
-
-    // const carGet = await passengerProfile.createCar()
-    // carGet.destroy(carGet)
-
-    response.status(201).json({ message: 'profile created!' })
+    await Promise.all([
+      ...Cars.map((car) => profile.createCar(car)),
+      ...TrustedBuddies.map((buddy) => profile.createTrustedbuddy(buddy)),
+      ...Card.map((card) => profile.createCard(card)),
+    ])
+    res.status(201).json({ message: 'Profile created!' })
   } catch (err) {
-    console.log(err)
-    response.status(400).json({ message: '', status: false })
+    console.error(err)
+    res.status(400).json({ message: 'Failed to create profile', status: false })
   }
 })
 
