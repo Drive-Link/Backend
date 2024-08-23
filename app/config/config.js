11f5c90813390b8e08dcc15bf06292b7cfb0df8d
Drive-Link <150278551+Drive-Link@users.noreply.github.com>
2024-08-23 18:58:21 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME && 'drive_link',
-    host: process.env.DB_HOST || 'host.docker.internal',
+    host: process.env.DB_HOST && 'host.docker.internal',
     dialect: 'mysql',
     port: process.env.DB_PORT,
   },
